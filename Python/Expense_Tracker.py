a664d710109f2972bd7c980a0d7f5df160aa42c8
Nathaniel S <ns.aaron@outlook.com>
2025-02-07 20:54:00 -0600

    Updated functions

--- Python/Expense_Tracker.py
+++ Python/Expense_Tracker.py
@@ -1,7 +1,9 @@
+print("Welcome to the Expense Tracker!")
+user_options = ["Add a new expense", "View all expenses", "View total expenses", "View expenses by category", "Exit"]
 class Expense:
     
     def __init__(self):
-        self.ammount = 50.0
+        self.amount = 50.0
         self.description = ""
         self.category= "" 
     
@@ -10,11 +12,14 @@ class ExpenseTracker:
     def __init__(self):
         self.expenses = []
         
-    def add_expense(description, amount, category):
-        print("Test")
+    def add_expense(self, expense):
+        item.description = input("Enter a Description of the expense: ")
+        item.amount = float(input("Enter the amount:"))
+        item.category = input("Enter the category:")
+        tracker.expenses.append(item)
         
-    def view_all_expenses():
-        print("Test")
+    def view_all_expenses(self):
+        print("PLACEHOLDER")
         
     def view_total_expenses():
         print("Test")
@@ -26,6 +31,28 @@ class ExpenseTracker:
 ##main  
 if __name__ == "__main__":
     tracker = ExpenseTracker()
-    expense = Expense()
+    item = Expense()
     
-    print("Welcome to the Expense Tracker!")
+    while True:
+        print("What would you like to do?")
+        counter = 1
+        for options in user_options:
+            print(f'{counter}- {options}')
+            counter += 1
+        user_selection = int(input("Enter your choice:"))
+        
+        if user_selection == 1:
+            tracker.add_expense(item)
+            
+        elif user_selection == 2:
+            tracker.view_all_expenses()
+            
+        elif user_selection == 3:
+            print("placeholder")
+            
+        elif user_selection == 4:
+            print("placeholder")
+            
+        elif user_selection == 5:
+            break
+    
